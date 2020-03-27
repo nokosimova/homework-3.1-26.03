373bf20ce2133222f2e7384c88587e48e2a90b7c
unknown <nkosimova06@gmail.com>
2020-03-27 20:11:06 +0500

    HW3 first try

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the sum:");
+            double init_sum    = double.Parse(Console.ReadLine());
+            double first_sale  = init_sum * 0.03;
+            double second_sale = init_sum * 0.05;
+            double total_sum;
+            if (init_sum <= 500) {
+                total_sum = init_sum;
+            } else {
+                total_sum = (init_sum > 500 && init_sum <= 1000)?
+                            init_sum-first_sale:init_sum-second_sale;
+            }
+            Console.WriteLine($"{total_sum}");
         }
     }
 }
